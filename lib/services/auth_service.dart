c0b6522dae6dbb05f30c3853a55449bf2e6f5533
Hugo <pv.hugom@gmail.com>
2025-01-11 19:11:55 +0100

    feat: add api call for login and signup

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,33 +1,28 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 import 'package:pixcellz/utils/shared_prefs_manager.dart';
 
 class AuthService {
   Future<Response> login(String email, String password) async {
-    //TODO: Uncomment this code when api is ready
-    // String api = "http://localhost:3000";
-    // Uri apiUrl = Uri.parse('$api/login');
-    // final response = await http.post(
-    //   apiUrl,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: jsonEncode({
-    //     'email': email,
-    //     'password': password,
-    //   }),
-    // );
-
-    final response = Response('''{
-  "session_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWYzZjQwZjQwZjQwZjQwZjQwZjQwZjQwIiwic2Vzc2lvbl90b2tlbiI6IjE2Mjg4NzYwNzIwNzIiLCJpYXQiOjE2Mjg4NzYwNzJ9.1",
-  "user_id": "5f3f40f40f40f40f40f40f40"
-}''', 200);
+    String api = "http://localhost:5001";
+    Uri apiUrl = Uri.parse('$api/api/auth/signin');
+    final response = await http.post(
+      apiUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+      }),
+    );
 
     final responseData = json.decode(response.body);
 
     if (response.statusCode == 200) {
-      final String sessionToken = responseData['session_token'];
-      final String userId = responseData['user_id'];
+      final String sessionToken = responseData['token'];
+      final String userId = responseData["user"]['_id'];
       await SharedPreferencesManager.loginUser(userId, sessionToken);
     } else {
       if (response.statusCode == 401) {
@@ -39,31 +34,25 @@ class AuthService {
   }
 
   Future<Response> signup(String username, String email, String password) async {
-    //TODO: Uncomment this code when api is ready
-    // String api = "http://localhost:3000";
-    // Uri apiUrl = Uri.parse('$api/signup');
-    // final response = await http.post(
-    //   apiUrl,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   body: jsonEncode({
-    //     'username': username,
-    //     'email': email,
-    //     'password': password,
-    //   }),
-    // );
-
-    final response = Response('''{
-  "session_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWYzZjQwZjQwZjQwZjQwZjQwZjQwZjQwIiwic2Vzc2lvbl90b2tlbiI6IjE2Mjg4NzYwNzIwNzIiLCJpYXQiOjE2Mjg4NzYwNzJ9.1",
-  "user_id": "5f3f40f40f40f40f40f40f40"
-}''', 201);
+    String api = "http://localhost:5001";
+    Uri apiUrl = Uri.parse('$api/api/auth/signup');
+    final response = await http.post(
+      apiUrl,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'username': username,
+        'email': email,
+        'password': password,
+      }),
+    );
 
     final responseData = json.decode(response.body);
 
     if (response.statusCode == 201) {
-      final String sessionToken = responseData['session_token'];
-      final String userId = responseData['user_id'];
+      final String sessionToken = responseData['token'];
+      final String userId = responseData['user']['_id'];
       await SharedPreferencesManager.loginUser(userId, sessionToken);
     } else {
       if (response.statusCode == 409) {
